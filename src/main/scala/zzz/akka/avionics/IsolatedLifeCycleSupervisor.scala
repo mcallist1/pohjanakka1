7212ff53923f33cc48f895aa7e1786c3b721b396
dylan drummond <dylan.drummond@gmail.com>
2016-03-16 12:56:28 +0200

    Add supervisor classes and traits

---
+++ src/main/scala/zzz/akka/avionics/IsolatedLifeCycleSupervisor.scala
@@ -0,0 +1,63 @@
+
+
+object IsolatedLifeCycleSupervisor {
+  // Messages in case we want people to be able to wait for us to finish starting:
+  case object WaitForStart
+  case object Started
+}
+
+trait IsolatedLifeCycleSupervisor extends Actor {
+  import IsolatedLifeCycleSupervisor._
+
+  def receive = {
+  // Emit Started msg:
+  case WaitForStart =>
+    sender ! Started
+  
+  // We don't handle anything else, but we give an error message stating the error:
+  case m => 
+    throw new Exception(s"Don't call ${self.path.name} directly ($m).")
+  }
+
+  // Subclass has to implement:
+  def childStarter(): Unit
+
+  // Only start children once we have started:
+  final override def preStart() { childStarter() }
+
+  // Don't call preStart(), which would otherwise be the default behaviour:
+  final override def postRestart(reason: Throwable) {}
+
+  // Don't stop the children, which would otherwise be the default behaviour:
+  final override def preRestart(reason: Throwable, message: Option[Any]) {}
+}
+
+abstract class IsolatedResumeSupervisor(
+        maxNrRetries: Int = -1,
+        withinTimeRange: Duration = Duration.Inf)
+      extends IsolatedLifeCycleSupervisor {
+  this: SupervisionStrategyFactory =>
+
+  override val supervisorStrategy = makeStrategy(maxNrRetries, withinTimeRange) {
+    case _: ActorInitializationException => Stop
+    case _: ActorKilledException         => Stop
+    case _: Exception                    => Resume
+    case _                               => Escalate
+  }
+}
+
+abstract class IsolatedStopSupervisor(
+        maxNrRetries: Int = -1,
+        withinTimeRange: Duration = Duration.Inf)
+      extends IsolatedLifeCycleSupervisor {
+  this: SupervisionStrategyFactory =>
+
+  override val supervisorStrategy = makeStrategy(maxNrRetries, withinTimeRange) {
+    case _: ActorInitializationException => Stop
+    case _: ActorKilledException         => Stop
+    case _: Exception                    => Stop
+    case _                               => Escalate
+  }
+}
+
+
---
+++ src/main/scala/zzz/akka/avionics/SupervisionStrategyFactory.scala
@@ -0,0 +1,24 @@
+import akka.actor.{SupervisorStrategy, OneForOneStrategy, AllForOneStrategy}
+import akka.actor.SupervisorStrategy._
+import scala.concurrent.duration.Duration
+
+trait SupervisionStrategyFactory {
+  def makeStrategy(
+    maxNrRetries: Int,
+    withinTimeRange: Duration
+  )(decider: Decider): SupervisorStrategy
+}
+
+trait OneForOneStrategyFactory extends SupervisionStrategyFactory {
+  def makeStrategy(
+    maxNrRetries: Int,
+    withinTimeRange: Duration
+  )(decider: Decider): SupervisorStrategy = OneForOneStrategy(maxNrRetries, withinTimeRange)(decider)
+
+trait AllForOneStrategyFactory extends SupervisionStrategyFactory {
+  def makeStrategy(
+    maxNrRetries: Int,
+    withinTimeRange: Duration
+  )(decider: Decider): SupervisorStrategy = AllForOneStrategy(maxNrRetries, withinTimeRange)(decider)
+
+
